e4c47b3dd3e40ac4ce738fc75550395f137b0982
Ronno777 <smithryan954@gmail.com>
2025-10-06 00:06:26 -0700

    ++ update

--- Build/Battle-Royale.loader.js
+++ Build/Battle-Royale.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 101735168,
+    wasmFileSize: 101734931,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
